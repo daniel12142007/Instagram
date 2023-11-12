4db4d84ca9fd3e4e486a8e416762faffd5cc41d6
daniel12142007 <danijelahatzanov@gmail.com>
2023-11-12 16:36:45 +0600

    I add checking for register method

--- src/main/java/com/example/instagram/dto/request/RegisterUserRequest.java
+++ src/main/java/com/example/instagram/dto/request/RegisterUserRequest.java
@@ -3,8 +3,7 @@ package com.example.instagram.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 // TODO: 9
 @Data
@@ -12,14 +11,17 @@ import javax.validation.constraints.NotNull;
 public class RegisterUserRequest {
     @NotNull(message = "Your fullName it is null!")
     @NotBlank(message = "Your fullName it is blank!")
-    @NotBlank(message = "Your fullName it is empty!")
+    @NotEmpty(message = "Your fullName it is empty!")
     private String fullName;
     @NotNull(message = "Your email it is null!")
     @NotBlank(message = "Your email it is blank!")
-    @NotBlank(message = "Your email it is empty!")
+    @NotEmpty(message = "Your email it is empty!")
+    @Pattern(regexp = ".*@gmail.com$")
     private String email;
     @NotNull(message = "Your password it is null!")
     @NotBlank(message = "Your password it is empty!")
     @NotBlank(message = "Your password it is blank!")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "The password does not meet the conditions")
+    @Size(min = 8, message = "The line length must be at least 8 characters.")
     private String password;
 }
