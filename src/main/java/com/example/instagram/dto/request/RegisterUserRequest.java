71327dc72f838080bd6ed01371e9eca02655ad98
daniel12142007 <danijelahatzanov@gmail.com>
2023-12-25 16:19:15 +0600

    I add pdf

--- src/main/java/com/example/instagram/dto/request/RegisterUserRequest.java
+++ src/main/java/com/example/instagram/dto/request/RegisterUserRequest.java
@@ -8,15 +8,12 @@ import javax.validation.constraints.*;
 @Data
 public class RegisterUserRequest {
     @NotNull(message = "Your fullName it is null!")
-    @NotBlank(message = "Your fullName it is blank!")
     @NotEmpty(message = "Your fullName it is empty!")
     private String fullName;
     @NotNull(message = "Your email it is null!")
-    @NotBlank(message = "Your email it is blank!")
     @NotEmpty(message = "Your email it is empty!")
     private String email;
     @NotNull(message = "Your password it is null!")
-    @NotBlank(message = "Your password it is empty!")
     @NotBlank(message = "Your password it is blank!")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "The password does not meet the conditions")
     @Size(min = 8, message = "The line length must be at least 8 characters.")
